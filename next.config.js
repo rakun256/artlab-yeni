b5dd7b3b831c7237ecd259599ac199edc676d404
Osman Bahadır Avcı <koixo@Osmans-MacBook-Pro-2.local>
2023-11-10 22:15:28 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['localhost', 'placehold.co'],
   },
-  output: 'export',
 };
 
 module.exports = nextConfig;
